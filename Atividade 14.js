ab5e1964eb3339f31b264bf26a63e3b44fefd1cb
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 18:50:41 -0300

    Update Atividade 14.js

--- Atividade 14.js
+++ Atividade 14.js
@@ -1,1 +1,31 @@
+//                                                            ATIVIDADE 14
 
+
+
+// // Objeto empresa com a propriedade departamentos
+
+// const empresa = {
+//     departamentos: [
+//         {
+//             nome: 'Recursos Humanos',
+//             funcionarios: ['Ana', 'Carlos', 'Beatriz']
+//         },
+//         {
+//             nome: 'TI',
+//             funcionarios: ['João', 'Mariana', 'Pedro']
+//         },
+//         {
+//             nome: 'Marketing',
+//             funcionarios: ['Fernanda', 'Lucas', 'Rafael']
+//         }
+//     ]
+// }
+
+// // Iterando sobre os departamentos e seus funcionários
+
+// for (const indice in empresa.departamentos) {
+//     const departamento = empresa.departamentos[indice]
+//     for (const funcionario of departamento.funcionarios) {
+//         console.log(`Funcionário: ${funcionario}, Departamento: ${departamento.nome}`)
+//     }
+// }
