de1a00da78dbf65bc92a19812fa470d31be60233
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 18:48:17 -0300

    Update Atividade 07.js

--- Atividade 07.js
+++ Atividade 07.js
@@ -1,1 +1,26 @@
+//                                                            ATIVIDADE 07
+
+
+
+// // Array de objetos produtos
+
+// let produtos = [
+//     { nome: "Monitor AOC", preco: 851.99, desconto: 0 },
+//     { nome: "Mouse Redragon", preco: 200.00, desconto: 0 },
+//     { nome: "Notebook Dell G15", preco: 5800.00, desconto: 0 }
+// ]
+
+// // Aplicar desconto de 10% em todos os produtos
+
+// produtos.forEach(produto => {
+//     produto.desconto = produto.preco * 0.10
+//     produto.preco = produto.preco - produto.desconto
+// })
+
+// // Exibir o novo preço dos produtos
+
+// produtos.forEach(produto => {
+//     console.log(`Nome: ${produto.nome}, Novo Preço: R$${produto.preco.toFixed(2)}`);
+// });
+
 
