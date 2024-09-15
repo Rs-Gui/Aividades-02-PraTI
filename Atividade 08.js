229c182e2cb8569fe36afb85867563b67b6b0807
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 18:48:35 -0300

    Update Atividade 08.js

--- Atividade 08.js
+++ Atividade 08.js
@@ -1,1 +1,25 @@
+//                                                            ATIVIDADE 08
+
+
+
+// // Array de objetos filmes
+
+// let filmes = [
+//     { titulo: "O Poderoso Chefão", diretor: "Francis Ford Coppola", anoLancamento: 1972 },
+//     { titulo: "Pulp Fiction", diretor: "Quentin Tarantino", anoLancamento: 1994 },
+//     { titulo: "A Origem", diretor: "Christopher Nolan", anoLancamento: 2010 }
+// ]
+
+// // Novo array para armazenar apenas os títulos dos filmes
+// let titulos = []
+
+// // Usar forEach para preencher o novo array com os títulos
+
+// filmes.forEach(filme => {
+//     titulos.push(filme.titulo)
+// });
+
+// // Exibir os títulos dos filmes
+
+// console.log(titulos)
 
