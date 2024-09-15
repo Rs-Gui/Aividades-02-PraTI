6609dc2722a987f158b2b7d2ded2f908e6aa66fa
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 18:50:21 -0300

    Update Atividade 13.js

--- Atividade 13.js
+++ Atividade 13.js
@@ -1,1 +1,26 @@
+//                                                            ATIVIDADE 13
+
+
+
+// // Objeto carrinho com a propriedade itens
+
+// const carrinho = {
+//     itens: [
+//         { nome: 'Maçã', quantidade: 3, precoUnitario: 2.00 },
+//         { nome: 'Banana', quantidade: 2, precoUnitario: 1.50 },
+//         { nome: 'Laranja', quantidade: 5, precoUnitario: 1.20 }
+//     ]
+// }
+
+// // Variável para armazenar o valor total do carrinho
+
+// let valorTotal = 0
+
+// // Usando forEach para calcular o valor total do carrinho
+
+// carrinho.itens.forEach(item => {
+//     valorTotal += item.quantidade * item.precoUnitario;
+// })
+
+// console.log(`Valor total do carrinho: R$ ${valorTotal.toFixed(2)}`)
 
