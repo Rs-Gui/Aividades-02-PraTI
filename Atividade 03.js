eff2fe5309e0e9b8363bf74b8b29e6b3555610b8
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 18:46:56 -0300

    Update Atividade 03.js

--- Atividade 03.js
+++ Atividade 03.js
@@ -1,1 +1,31 @@
+//                                                            ATIVIDADE 03
 
+
+
+// Função para filtrar propriedades do objeto
+
+// function filtrarPropriedades(obj, valorMinimo) {
+//     let novoObjeto = {}
+
+//     for (let propriedade in obj) {
+//         if (obj[propriedade] > valorMinimo) {
+//             novoObjeto[propriedade] = obj[propriedade]
+//         }
+//     }
+
+//     return novoObjeto
+// }
+
+// // Exemplo de uso
+
+// let produto = {
+//     preco: 150,
+//     peso: 2,
+//     quantidade: 10,
+//     desconto: 5
+// }
+
+// let valorMinimo = 5
+// let resultado = filtrarPropriedades(produto, valorMinimo)
+
+// console.log(resultado)
