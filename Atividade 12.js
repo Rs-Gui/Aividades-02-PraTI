7b4ec2388a28c10fac624476c9886def98fae5b3
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 18:49:58 -0300

    Update Atividade 12.js

--- Atividade 12.js
+++ Atividade 12.js
@@ -1,1 +1,32 @@
+//                                                            ATIVIDADE 12
+
+
+
+// // Array do estoque de objetos
+
+// const estoque = [
+
+//     {produto: 'Brinco', quantidade: 10, minimo: 8},
+//     {produto: 'Pulseira', quantidade: 4, minimo: 5},
+//     {produto: 'Anéis', quantidade: 18, minimo: 25},
+//     {produto: 'Colar', quantidade: 1, minimo: 5},
+//     {produto: 'Relógio', quantidade: 10, minimo: 5}
+
+// ]
+
+// // Usando forEach para duplicar os produtos necessários
+
+// estoque.forEach(produtos => {
+
+//     while (produtos.quantidade < produtos.minimo) {
+
+//         produtos.quantidade *= 2
+
+//     }
+
+// })
+
+// // Exibindo os resultados
+
+// console.log(estoque)
 
