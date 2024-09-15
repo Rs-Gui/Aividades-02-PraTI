6c3f36c563ef2f6cd4632f793382922b3afcab99
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 18:47:17 -0300

    Update Atividade 04.js

--- Atividade 04.js
+++ Atividade 04.js
@@ -1,1 +1,18 @@
+//                                                            ATIVIDADE 04
 
+
+
+// // Criação do array de objetos pessoas
+
+// let pessoas = [
+//     { nome: "Ana", idade: 28, cidade: "São Paulo" },
+//     { nome: "Bruno", idade: 34, cidade: "Rio de Janeiro" },
+//     { nome: "Carla", idade: 25, cidade: "Belo Horizonte" },
+//     { nome: "Diego", idade: 30, cidade: "Porto Alegre" }
+// ]
+
+// // Iteração sobre o array de objetos pessoas
+
+// for (let pessoa of pessoas) {
+//     console.log(`Nome: ${pessoa.nome}, Idade: ${pessoa.idade}, Cidade: ${pessoa.cidade}`)
+// }
