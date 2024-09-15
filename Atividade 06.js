cea6ddd515d192b1a4fedd866c92db65fa200e4d
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 18:47:56 -0300

    Update Atividade 06.js

--- Atividade 06.js
+++ Atividade 06.js
@@ -1,1 +1,30 @@
+//                                                            ATIVIDADE 06
+
+
+// // Criação do array de objetos funcionários
+
+// let funcionarios = [
+
+//     {nome: 'Maria', cargo: 'Consultora Junior', salario: 2300},
+//     {nome: 'Jõao', cargo: 'Consultor Pleno', salario: 3000},
+//     {nome: 'Cleber', cargo: 'Vendedor', salario: 1800},
+//     {nome: 'Joana', cargo: 'Analista de Dados', salario: 4300},
+//     {nome: 'Marcelo', cargo: 'Faxineiro', salario: 1500},
+//     {nome: 'Kauan', cargo: 'Consultor Junior', salario: 2300},
+//     {nome: 'Rose', cargo: 'Vendedora Executiva', salario: 4800},
+//     {nome: 'Raissa', cargo: 'Gerente', salario: 4000},
+//     {nome: 'Guilherme', cargo: 'Vendedor', salario: 1800}
+
+// ]
+
+// // Criação do filtro salárial
+
+// for(funcionario of funcionarios) {
+
+//     if (funcionario.salario >= 2000)
+
+//         console.log(`Nome: ${funcionario.nome}, Cargo: ${funcionario.cargo}, Salário: ${funcionario.salario}`)
+
+// }
+
 
