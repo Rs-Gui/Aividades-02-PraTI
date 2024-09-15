9925c6990c1bb4d9d9829091a890db982b290095
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 18:51:02 -0300

    Update Atividade 15.js

--- Atividade 15.js
+++ Atividade 15.js
@@ -1,1 +1,40 @@
+//                                                            ATIVIDADE 15
 
+
+
+// // Declaração de variável do saldo da conta
+
+// let saldoDaConta = 2000
+
+// // Array de objeto transações 
+
+// const transacoes = [
+//     {tipo: 'Saída', valor: 200},
+//     {tipo: 'Saída', valor: 250},
+//     {tipo: 'Saída', valor: 20},
+//     {tipo: 'Saída', valor: 100},
+//     {tipo: 'Entrada', valor: 300},
+//     {tipo: 'Saída', valor: 180},
+//     {tipo: 'Saída', valor: 80},
+//     {tipo: 'Entrada', valor: 100}
+// ]
+
+// // Usando forEach para diminuir ou aumentar o saldo da conta com base no tipo de transação 
+
+// transacoes.forEach(transação => {
+
+//     if(transação.tipo === 'Saída') {
+
+//         saldoDaConta -= transação.valor
+
+//     } else {
+
+//         saldoDaConta += transação.valor
+
+//     }
+
+// })
+
+// // Exibindo resultados
+
+// console.log(saldoDaConta)
