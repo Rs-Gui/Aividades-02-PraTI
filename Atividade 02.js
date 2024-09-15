ba7667495e894303bb9636a87d93a78c79bb8556
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 18:46:35 -0300

    Update Atividade 02.js

--- Atividade 02.js
+++ Atividade 02.js
@@ -1,1 +1,29 @@
+//                                                            ATIVIDADE 02
 
+
+
+// // Criação do objeto livro
+
+// let livro = {
+//     titulo: "Dom Casmurro",
+//     autor: "Machado de Assis",
+//     anoPublicacao: 1899,
+//     genero: "Romance"
+// };
+
+// // Verificação e adição da propriedade editora
+
+// let propriedadeExiste = false
+
+// for (let propriedade in livro) {
+//     if (propriedade === "editora") {
+//         propriedadeExiste = true
+//         break
+//     }
+// }
+
+// if (!propriedadeExiste) {
+//     livro.editora = "Grupo Companhia das Letras"
+// }
+
+// console.log(livro)
