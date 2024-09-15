451504c8e52b1f009d826e368c48bdc5128c3fc5
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 18:47:38 -0300

    Update Atividade 05.js

--- Atividade 05.js
+++ Atividade 05.js
@@ -1,1 +1,20 @@
+//                                                            ATIVIDADE 05
+
+
+
+// // Criação do array de objetos alunos
+
+// let alunos = [
+//     { nome: "João", nota1: 8, nota2: 7 },
+//     { nome: "Maria", nota1: 9, nota2: 8 },
+//     { nome: "Pedro", nota1: 6, nota2: 5 },
+//     { nome: "Ana", nota1: 10, nota2: 9 }
+// ]
+
+// // Iteração sobre o array de objetos alunos e cálculo da média
+
+// for (let aluno of alunos) {
+//     let media = (aluno.nota1 + aluno.nota2) / 2
+//     console.log(`Nome: ${aluno.nome}, Média: ${media}`)
+// }
 
