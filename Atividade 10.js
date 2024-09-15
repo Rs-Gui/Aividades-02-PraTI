783faa4ad190ba182e9d206448b9323d53d11766
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 18:49:17 -0300

    Update Atividade 10.js

--- Atividade 10.js
+++ Atividade 10.js
@@ -1,1 +1,26 @@
+//                                                            ATIVIDADE 10
+
+
+
+// // Array de objetos vendas
+
+// let vendas = [
+//     { produto: "Banana", quantidade: 2, valor: 50 },
+//     { produto: "maça nanica", quantidade: 1, valor: 100 },
+//     { produto: "Uva", quantidade: 3, valor: 30 }
+// ]
+
+// // Variável para armazenar o valor total das vendas
+
+// let valorTotal = 0
+
+// // Usar forEach para calcular o valor total das vendas
+
+// vendas.forEach(venda => {
+//     valorTotal += venda.quantidade * venda.valor
+// })
+
+// // Exibir o valor total das vendas
+
+// console.log(`Valor total das vendas: R$${valorTotal.toFixed(2)}`)
 
