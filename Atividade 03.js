b6c28880cf82cc8e5e7079d108cb0ce2c0e6f2da
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 21:53:44 -0300

    Update Atividade 03.js

--- Atividade 03.js
+++ Atividade 03.js
@@ -1,31 +1,34 @@
 //                                                            ATIVIDADE 03
 
-
+// Objetivo: Dado um objeto produto com várias propriedades, crie uma função que retorna um novo objeto contendo apenas as propriedades cujo valor seja maior que um valor específico. 
+// Use for in para filtrar as propriedades.
 
 // Função para filtrar propriedades do objeto
 
-// function filtrarPropriedades(obj, valorMinimo) {
-//     let novoObjeto = {}
+function filtrarPropriedades(obj, valorMinimo) {
+    let novoObjeto = {}
 
-//     for (let propriedade in obj) {
-//         if (obj[propriedade] > valorMinimo) {
-//             novoObjeto[propriedade] = obj[propriedade]
-//         }
-//     }
+    for (let propriedade in obj) {
+        if (obj[propriedade] > valorMinimo) {
+            novoObjeto[propriedade] = obj[propriedade]
+        }
+    }
 
-//     return novoObjeto
-// }
+    return novoObjeto
+}
 
-// // Exemplo de uso
+// Exemplo de uso
 
-// let produto = {
-//     preco: 150,
-//     peso: 2,
-//     quantidade: 10,
-//     desconto: 5
-// }
+let produto = {
+    preco: 150,
+    peso: 2,
+    quantidade: 10,
+    desconto: 5
+}
 
-// let valorMinimo = 5
-// let resultado = filtrarPropriedades(produto, valorMinimo)
+let valorMinimo = 5
+let resultado = filtrarPropriedades(produto, valorMinimo)
 
-// console.log(resultado)
+// Exibição do resultado final
+
+console.log(resultado)
