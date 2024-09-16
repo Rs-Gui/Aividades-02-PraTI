5b9c832a4a37b60fad25357fda900764a4ea5dac
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 22:01:17 -0300

    Update Atividade 10.js

--- Atividade 10.js
+++ Atividade 10.js
@@ -1,26 +1,26 @@
 //                                                            ATIVIDADE 10
 
+// Objetivo: Crie um array de objetos vendas, onde cada objeto tem produto, quantidade e valor. Use forEach para calcular o valor total de vendas de todos os produtos.
 
+// Array de objetos vendas
 
-// // Array de objetos vendas
+let vendas = [
+    { produto: "Banana", quantidade: 2, valor: 50 },
+    { produto: "maça nanica", quantidade: 1, valor: 100 },
+    { produto: "Uva", quantidade: 3, valor: 30 }
+]
 
-// let vendas = [
-//     { produto: "Banana", quantidade: 2, valor: 50 },
-//     { produto: "maça nanica", quantidade: 1, valor: 100 },
-//     { produto: "Uva", quantidade: 3, valor: 30 }
-// ]
+// Variável para armazenar o valor total das vendas
 
-// // Variável para armazenar o valor total das vendas
+let valorTotal = 0
 
-// let valorTotal = 0
+// Usar forEach para calcular o valor total das vendas
 
-// // Usar forEach para calcular o valor total das vendas
+vendas.forEach(venda => {
+    valorTotal += venda.quantidade * venda.valor
+})
 
-// vendas.forEach(venda => {
-//     valorTotal += venda.quantidade * venda.valor
-// })
+// Exibir o valor total das vendas
 
-// // Exibir o valor total das vendas
-
-// console.log(`Valor total das vendas: R$${valorTotal.toFixed(2)}`)
+console.log(`Valor total das vendas: R$${valorTotal.toFixed(2)}`)
 
