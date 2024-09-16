05d040bd073173196ae56223f79dd29836ece1f1
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 21:49:02 -0300

    Update Atividade 01.js

--- Atividade 01.js
+++ Atividade 01.js
@@ -1,18 +1,18 @@
 //                                                            ATIVIDADE 01 
 
+// Objetivo: Crie um objeto carro com propriedades como marca, modelo, ano, e cor. Use for in para iterar sobre as propriedades e exibir seus valores no console.
 
+// Criação do objeto carro
 
-// // Criação do objeto carro
+let carro = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    ano: 2021,
+    cor: "Prata"
+};
 
-// let carro = {
-//     marca: "Toyota",
-//     modelo: "Corolla",
-//     ano: 2021,
-//     cor: "Prata"
-// };
+// Iteração sobre as propriedades do objeto carro
 
-// // Iteração sobre as propriedades do objeto carro
-
-// for (let propriedade in carro) {
-//     console.log(`${propriedade}: ${carro[propriedade]}`)
-// }
+for (let propriedade in carro) {
+    console.log(`${propriedade}: ${carro[propriedade]}`)
+}
