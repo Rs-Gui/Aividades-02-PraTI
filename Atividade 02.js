3647317658c4e82f24f06621705052e81ae0b733
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 21:51:34 -0300

    Update Atividade 02.js

--- Atividade 02.js
+++ Atividade 02.js
@@ -1,29 +1,31 @@
 //                                                            ATIVIDADE 02
 
+// Objetivo: Crie um objeto livro com propriedades titulo, autor, anoPublicacao e genero. Verifique se a propriedade editora existe no objeto usando for in. Se não existir, adicione essa propriedade ao objeto.
 
+// Criação do objeto livro
 
-// // Criação do objeto livro
+let livro = {
+    titulo: "Dom Casmurro",
+    autor: "Machado de Assis",
+    anoPublicacao: 1899,
+    genero: "Romance"
+};
 
-// let livro = {
-//     titulo: "Dom Casmurro",
-//     autor: "Machado de Assis",
-//     anoPublicacao: 1899,
-//     genero: "Romance"
-// };
+// Verificação e adição da propriedade editora
 
-// // Verificação e adição da propriedade editora
+let propriedadeExiste = false
 
-// let propriedadeExiste = false
+for (let propriedade in livro) {
+    if (propriedade === "editora") {
+        propriedadeExiste = true
+        break
+    }
+}
 
-// for (let propriedade in livro) {
-//     if (propriedade === "editora") {
-//         propriedadeExiste = true
-//         break
-//     }
-// }
+if (!propriedadeExiste) {
+    livro.editora = "Grupo Companhia das Letras"
+}
 
-// if (!propriedadeExiste) {
-//     livro.editora = "Grupo Companhia das Letras"
-// }
+// Exibição do resultado final
 
-// console.log(livro)
+console.log(livro)
