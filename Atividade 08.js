6772b64a931da7809f73967bc301808f3c1b1720
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 21:59:48 -0300

    Update Atividade 08.js

--- Atividade 08.js
+++ Atividade 08.js
@@ -1,25 +1,25 @@
 //                                                            ATIVIDADE 08
 
+// Objetivo: Crie um array de objetos filmes, onde cada filme tem titulo, diretor, e anoLancamento. Use forEach para criar um novo array contendo apenas os títulos dos filmes. 
 
+// Array de objetos filmes
 
-// // Array de objetos filmes
+let filmes = [
+    { titulo: "O Poderoso Chefão", diretor: "Francis Ford Coppola", anoLancamento: 1972 },
+    { titulo: "Pulp Fiction", diretor: "Quentin Tarantino", anoLancamento: 1994 },
+    { titulo: "A Origem", diretor: "Christopher Nolan", anoLancamento: 2010 }
+]
 
-// let filmes = [
-//     { titulo: "O Poderoso Chefão", diretor: "Francis Ford Coppola", anoLancamento: 1972 },
-//     { titulo: "Pulp Fiction", diretor: "Quentin Tarantino", anoLancamento: 1994 },
-//     { titulo: "A Origem", diretor: "Christopher Nolan", anoLancamento: 2010 }
-// ]
+// Novo array para armazenar apenas os títulos dos filmes
+let titulos = []
 
-// // Novo array para armazenar apenas os títulos dos filmes
-// let titulos = []
+// Usar forEach para preencher o novo array com os títulos
 
-// // Usar forEach para preencher o novo array com os títulos
+filmes.forEach(filme => {
+    titulos.push(filme.titulo)
+});
 
-// filmes.forEach(filme => {
-//     titulos.push(filme.titulo)
-// });
+// Exibir os títulos dos filmes
 
-// // Exibir os títulos dos filmes
-
-// console.log(titulos)
+console.log(titulos)
 
