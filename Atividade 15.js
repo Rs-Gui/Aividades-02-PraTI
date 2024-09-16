7eaee7583dffdd190d9ba39783ec2cbd4bc89075
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 22:06:33 -0300

    Update Atividade 15.js

--- Atividade 15.js
+++ Atividade 15.js
@@ -1,40 +1,40 @@
 //                                                            ATIVIDADE 15
 
+// Objetivo: Crie um array de objetos transacoes, onde cada transação tem tipo (entrada ou saída) e valor. Use forEach para calcular o saldo final, somando as entradas e subtraindo as saídas.
 
+// Declaração de variável do saldo da conta
 
-// // Declaração de variável do saldo da conta
+let saldoDaConta = 2000
 
-// let saldoDaConta = 2000
+// Array de objeto transações 
 
-// // Array de objeto transações 
+const transacoes = [
+    {tipo: 'Saída', valor: 200},
+    {tipo: 'Saída', valor: 250},
+    {tipo: 'Saída', valor: 20},
+    {tipo: 'Saída', valor: 100},
+    {tipo: 'Entrada', valor: 300},
+    {tipo: 'Saída', valor: 180},
+    {tipo: 'Saída', valor: 80},
+    {tipo: 'Entrada', valor: 100}
+]
 
-// const transacoes = [
-//     {tipo: 'Saída', valor: 200},
-//     {tipo: 'Saída', valor: 250},
-//     {tipo: 'Saída', valor: 20},
-//     {tipo: 'Saída', valor: 100},
-//     {tipo: 'Entrada', valor: 300},
-//     {tipo: 'Saída', valor: 180},
-//     {tipo: 'Saída', valor: 80},
-//     {tipo: 'Entrada', valor: 100}
-// ]
+// Usando forEach para diminuir ou aumentar o saldo da conta com base no tipo de transação 
 
-// // Usando forEach para diminuir ou aumentar o saldo da conta com base no tipo de transação 
+transacoes.forEach(transação => {
 
-// transacoes.forEach(transação => {
+    if(transação.tipo === 'Saída') {
 
-//     if(transação.tipo === 'Saída') {
+        saldoDaConta -= transação.valor
 
-//         saldoDaConta -= transação.valor
+    } else {
 
-//     } else {
+        saldoDaConta += transação.valor
 
-//         saldoDaConta += transação.valor
+    }
 
-//     }
+})
 
-// })
+// Exibindo resultados
 
-// // Exibindo resultados
-
-// console.log(saldoDaConta)
+console.log(saldoDaConta)
