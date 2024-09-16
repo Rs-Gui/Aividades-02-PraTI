4beb2b80d91f582c296537459cb41362f151f91c
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 22:04:07 -0300

    Update Atividade 13.js

--- Atividade 13.js
+++ Atividade 13.js
@@ -1,26 +1,29 @@
 //                                                            ATIVIDADE 13
 
+// Objetivo: Crie um objeto carrinho com uma propriedade itens, que é umarray de objetos. Cada objeto dentro de itens deve representar um produto no carrinho, com nome, quantidade, e precoUnitario. 
+// Use forEach para calcular o valor total do carrinho.
 
+// Objeto carrinho com a propriedade itens
 
-// // Objeto carrinho com a propriedade itens
+const carrinho = {
+    itens: [
+        { nome: 'Maçã', quantidade: 3, precoUnitario: 2.00 },
+        { nome: 'Banana', quantidade: 2, precoUnitario: 1.50 },
+        { nome: 'Laranja', quantidade: 5, precoUnitario: 1.20 }
+    ]
+}
 
-// const carrinho = {
-//     itens: [
-//         { nome: 'Maçã', quantidade: 3, precoUnitario: 2.00 },
-//         { nome: 'Banana', quantidade: 2, precoUnitario: 1.50 },
-//         { nome: 'Laranja', quantidade: 5, precoUnitario: 1.20 }
-//     ]
-// }
+// Variável para armazenar o valor total do carrinho
 
-// // Variável para armazenar o valor total do carrinho
+let valorTotal = 0
 
-// let valorTotal = 0
+// Usando forEach para calcular o valor total do carrinho
 
-// // Usando forEach para calcular o valor total do carrinho
+carrinho.itens.forEach(item => {
+    valorTotal += item.quantidade * item.precoUnitario;
+})
 
-// carrinho.itens.forEach(item => {
-//     valorTotal += item.quantidade * item.precoUnitario;
-// })
+// Exibição do resultado
 
-// console.log(`Valor total do carrinho: R$ ${valorTotal.toFixed(2)}`)
+console.log(`Valor total do carrinho: R$ ${valorTotal.toFixed(2)}`)
 
