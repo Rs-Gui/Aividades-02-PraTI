1e5331b15592f2cc8d1188e6a2039d74022703bf
Guilherme Rodrigues <165702743+Rs-Gui@users.noreply.github.com>
2024-09-15 22:02:52 -0300

    Update Atividade 12.js

--- Atividade 12.js
+++ Atividade 12.js
@@ -1,32 +1,32 @@
 //                                                            ATIVIDADE 12
 
+// Objetivo: Crie um array de objetos estoque, onde cada objeto tem produto, quantidade e minimo. Use forEach para atualizar a quantidade dos produtos que estão abaixo do mínimo, duplicando suas quantidades.
 
+// Array do estoque de objetos
 
-// // Array do estoque de objetos
+const estoque = [
 
-// const estoque = [
+    {produto: 'Brinco', quantidade: 10, minimo: 8},
+    {produto: 'Pulseira', quantidade: 4, minimo: 5},
+    {produto: 'Anéis', quantidade: 18, minimo: 25},
+    {produto: 'Colar', quantidade: 1, minimo: 5},
+    {produto: 'Relógio', quantidade: 10, minimo: 5}
 
-//     {produto: 'Brinco', quantidade: 10, minimo: 8},
-//     {produto: 'Pulseira', quantidade: 4, minimo: 5},
-//     {produto: 'Anéis', quantidade: 18, minimo: 25},
-//     {produto: 'Colar', quantidade: 1, minimo: 5},
-//     {produto: 'Relógio', quantidade: 10, minimo: 5}
+]
 
-// ]
+// Usando forEach para duplicar os produtos necessários
 
-// // Usando forEach para duplicar os produtos necessários
+estoque.forEach(produtos => {
 
-// estoque.forEach(produtos => {
+    while (produtos.quantidade < produtos.minimo) {
 
-//     while (produtos.quantidade < produtos.minimo) {
+        produtos.quantidade *= 2
 
-//         produtos.quantidade *= 2
+    }
 
-//     }
+})
 
-// })
+// Exibindo os resultados
 
-// // Exibindo os resultados
-
-// console.log(estoque)
+console.log(estoque)
 
